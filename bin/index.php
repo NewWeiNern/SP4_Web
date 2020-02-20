5448aa4100bc49945697786f65974cdcc3610bff
NewWeiNern <nwnpoly@gmail.com>
2020-02-20 18:24:38 +0800

    Ratchet library and bin update

--- bin/index.php
+++ bin/index.php
@@ -1,11 +1,12 @@
 <?php
-@$code = $_GET["code"];
-
 require_once "../php/class/bin.class.php";
+
+@$code = $_GET["code"];
 $bin = new Bin();
 $data = "";
 
 if($code){
+    // Get the location data
     $data = $bin->getLocationData($code)['bin_name'];
 }
 ?>
@@ -17,14 +18,21 @@ if($code){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= empty($data) ? "Compost Bin Not Found!" : "Compost Bin @".$data; ?></title>
     <script src="js/vendor/jquery-3.4.1.min.js"></script>
-    <script src="bin/js/main.js"></script>
 </head>
 <body>
     <?php if(!empty($code) && !empty($data)): ?>
     <input type="button" onclick="process()" value="Simulate Item in Collection Bin"/>
     <input type="hidden" value="<?= $code;?>" name="code_loc"/>
     <img id="qrCode" name="qrCode" style="height:150px; width:150px;"/>
-
+    <script>
+        let socket = new WebSocket("ws://localhost:8080/");
+        socket.onopen = e=>{
+            console.log(e);
+        }
+        socket.onmessage = e=>{
+            console.log(e);
+        }
+    </script>
     <?php else:?>
         Bin not found!
     <?php endif;?>
---
+++ php/socket.php
@@ -0,0 +1,63 @@
+<?php
+exec("C:\\xampp\php\php.exe socket.php");
+set_time_limit(0);
+
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
+
+require_once "./lib/Ratchet/autoload.php";
+
+class Chat implements MessageComponentInterface{
+    protected $clients;
+    protected $users;
+
+    public function __construct()
+    {
+        $this->clients = new \SplObjectStorage;
+    }
+
+    public function onOpen(\Ratchet\ConnectionInterface $conn)
+    {
+        $this->clients->attach($conn);
+    }
+
+    public function onClose(\Ratchet\ConnectionInterface $conn)
+    {
+        $this->clients->detach($conn);
+    }
+
+    public function onMessage(\Ratchet\ConnectionInterface $from, $msg)
+    {
+        $from_id = $from->resourceId;
+        $data = json_decode($msg);
+        $type = $data->type;
+
+        switch($type){
+            case "chat" :
+                $user_id = $data->user_id;
+                $chat_msg = $data->chat_msg;
+                $response_from = "<span style='color:#999'><b>".$user_id.":</b> ".$chat_msg."</span><br><br>";
+                $response_to = "<b>".$user_id."</b>: ".$chat_msg."<br><br>";
+                $from->send(json_encode(array("type"=>$type, "msg"=>$response_from)));
+
+                foreach($this->clients as $client){
+                    if($from != $client){
+                        $client->send(json_encode(array("type"=>$type, "msg"=>$response_to)));
+                    }
+                }
+            break;
+        }
+    }
+
+    public function onError(\Ratchet\ConnectionInterface $conn, \Exception $e)
+    {
+        $conn->close();
+    }
+}
+
+$server = IoServer::factory(new HttpServer(new WsServer(new Chat())), 8080);
+$server->run();
+?>
